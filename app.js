79fcba138ed6ac2eb0b5c95b7090b0622b196968
Rinalds Akulovs <rinsldsa@gmail.com>
2021-03-09 15:21:02 +0200

    Leason 5

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var titles = document.getElementsByClassName('title');
+//var titles = document.getElementsByClassName('title');
 
 //console.log(Array.isArray(titles));
 //console.log(Array.isArray(Array.from(titles)));
@@ -7,16 +7,25 @@ var titles = document.getElementsByClassName('title');
 //    console.log(item);
 //})
 
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+//const wmf = document.querySelector('#book-list li:nth-child(2) .name');
 //console.log(wmf);
 
-var books = document.querySelector('#book-list li .name')
+//var books = document.querySelector('#book-list li .name')
 //console.log(books);
 
 
-books = document.querySelectorAll('#book-list li .name')
+//books = document.querySelectorAll('#book-list li .name')
 //console.log(books);
+
+//Array.from(books).forEach(function(book){
+//    console.log(book);
+//})
+var books = document.querySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function(book){
-    console.log(book);
+    book.textContent += '(book title)';
 })
+
+const bookList = document.querySelector('#book-list')
+//bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML</p>';
