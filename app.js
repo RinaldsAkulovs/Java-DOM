954dc2b44ed2bf72f7a8ab538c413fd657f062bf
Rinalds Akulovs <rinsldsa@gmail.com>
2021-03-09 16:14:54 +0200

    Leason 9

--- app.js
+++ app.js
@@ -48,12 +48,30 @@
 
 //console.log(bookList.children);
 
-const bookList = document.querySelector('#book-list');
+//const bookList = document.querySelector('#book-list');
 
-console.log('#book-list next sibling is:',bookList.nextSibling);
-console.log('#book-list next element sibling is:',bookList.nextElementSibling);
+//console.log('#book-list next sibling is:',bookList.nextSibling);
+//console.log('#book-list next element sibling is:',bookList.nextElementSibling);
 
-console.log('#book-list previous sibling is:',bookList.previousSibling);
-console.log('#book-list previous element sibling is:',bookList.previousElementSibling);
+//console.log('#book-list previous sibling is:',bookList.previousSibling);
+//console.log('#book-list previous element sibling is:',bookList.previousElementSibling);
 
-bookList.previousElementSibling.querySelector('p').innerHTMP += '<br />Too cool for everyone else!:';
+//bookList.previousElementSibling.querySelector('p').innerHTMP += '<br />Too cool for everyone else!:';
+
+var btns = document.querySelectorAll('#book-list.delete');
+
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click',function(e){
+
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li)
+
+    });
+});
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click',function(e){
+    e.preventDefault();
+    console.log('navigate to', e.target.textContent, 'was prevented');
+})
