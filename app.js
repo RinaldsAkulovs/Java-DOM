6c40f42118798edc074ba4b8434f1b158c2f98a1
Rinalds Akulovs <rinsldsa@gmail.com>
2021-03-09 15:37:42 +0200

    Leason 7

--- app.js
+++ app.js
@@ -31,12 +31,19 @@
 //bookList.innerHTML = '<h2>Books and more books...</h2>';
 //bookList.innerHTML += '<p>This is how you add HTML</p>';
 
-const banner = document.querySelector('#page-banner');
+//const banner = document.querySelector('#page-banner');
 
-console.log('#page-banner node type is:', banner.nodeType);
-console.log('#page-banner node name is:', banner.nodeName);
-console.log('#page-banner has child nodes is:', banner.hasChildNodes())
+//console.log('#page-banner node type is:', banner.nodeType);
+//console.log('#page-banner node name is:', banner.nodeName);
+//console.log('#page-banner has child nodes is:', banner.hasChildNodes())
 
 
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
+//const clonedBanner = banner.cloneNode(true);
+//console.log(clonedBanner);
+
+const bookList = document.querySelector('#book-list');
+
+console.log('#the parent node is;',bookList.parentNode);
+console.log('#the parent element is;',bookList.parentElement.parentElement);
+
+console.log(bookList.children);
