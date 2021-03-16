6b6bd9049dbd5b6b5b1bda500b64d57861d2394e
Rinalds Akulovs <rinsldsa@gmail.com>
2021-03-16 14:27:15 +0200

    Leason 12

--- app.js
+++ app.js
@@ -91,10 +91,26 @@ list.addEventListener('click',function(e){
 })
 
 //add book-list
-const addForm = document.forms['add-book'];
-
+const addForm = document.forms['add-book']; 
 addForm.addEventListener('submit',function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
     console.log(value)
+
+    //create element
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content
+
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+    
+
+    // append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
 });
